575e5ca7f7589efcde38b1edcffe51241fb14d25
Yaroslav_Orlov <sanenss@gmail.com>
2024-09-27 19:23:33 +0300

    add release date

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -4,60 +4,61 @@ import { fetchMovieDetails } from "../../services/TMBDapi"
 import Loader from "../../components/Loader/Loader"
 
 const MovieDetailsPage = () => {
+  const { movieId } = useParams()
+  const [movieDetails, setMovieDetails] = useState(null)
+  const [error, setError] = useState(false)
+  const [loading, setIsLoading] = useState(false)
+  const location = useLocation()
+  const backLink = useRef(location.state ?? "/movies")
 
-const {movieId} = useParams()
-const [movieDetails,setMovieDetails] = useState(null)
-const [error,setError] = useState(false)
-const [loading,setIsLoading] = useState(false)
-const location = useLocation(); 
-const backLink = useRef(location.state ?? '/movies');
-
-useEffect(()=>{
-  const getMovieDetails = async ()=>{
-    try{
-      setError(false)
-      setIsLoading(true)
-      const data = await fetchMovieDetails(movieId)
-      setMovieDetails(data)
+  useEffect(() => {
+    const getMovieDetails = async () => {
+      try {
+        setError(false)
+        setIsLoading(true)
+        const data = await fetchMovieDetails(movieId)
+        setMovieDetails(data)
+      } catch {
+        setError(true)
+      } finally {
+        setIsLoading(false)
+      }
     }
-    catch{
-      setError(true)
-    }
-    finally{
-      setIsLoading(false)
-    }
-   
-  }
-  getMovieDetails()
-},[movieId])
-
-
+    getMovieDetails()
+  }, [movieId])
 
   return (
     <div>
-    <Link to={backLink.current}>Go back</Link>
-     {loading && <Loader/> }
-     {error && <div>Something went wrong, please try again</div> }
-     {movieDetails && (<div>
-       <img src={`https://image.tmdb.org/t/p/w500/${movieDetails.poster_path}`} alt={movieDetails.title} />
-       <h3>{movieDetails.title}</h3>
-       <p>Rating: {(movieDetails.vote_average).toFixed(1)}</p>
-       <p>Overview:{movieDetails.overview} </p>
-       <ul>
-        <h4>Genres</h4>
-        {movieDetails.genres.map(genre=> <li key={genre.id}>
-        <span>{genre.name}</span>
-        </li>)}
-      </ul>
-      </div>)}
-      <hr/>
+      <Link to={backLink.current}>Go back</Link>
+      {loading && <Loader />}
+      {error && <div>Something went wrong, please try again</div>}
+      {movieDetails && (
+        <div>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${movieDetails.poster_path}`}
+            alt={movieDetails.title}
+          />
+          <h3>{movieDetails.title}</h3>
+          <p>Rating: {movieDetails.vote_average.toFixed(1)}</p>
+          <p>Overview:{movieDetails.overview} </p>
+          <p>Date: {movieDetails.release_date}</p>
+          <ul>
+            <h4>Genres</h4>
+            {movieDetails.genres.map((genre) => (
+              <li key={genre.id}>
+                <span>{genre.name}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <hr />
       <div>
         <NavLink to="cast">Cast</NavLink>
         <NavLink to="reviews">Reviews</NavLink>
       </div>
-      <Outlet/>
-  </div>
-
+      <Outlet />
+    </div>
   )
 }
 
